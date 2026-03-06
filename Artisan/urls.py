aee0c798f20cb803d3a22e9a7923161a1d5033ea
AshfahAshraf <ashfahashraf@gmail.com>
2026-03-06 07:44:47 +0530

    updated project

--- Artisan/urls.py
+++ Artisan/urls.py
@@ -1,7 +1,3 @@
 from django.urls import path
 from .import views
-urlpatterns = [
-              path('seller_home',views.seller_home,name="seller_home")
-
-
-]
+urlpatterns = []
--- Artisan/views.py
+++ Artisan/views.py
@@ -2,5 +2,3 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def seller_home(request):
-    return render(request,"seller_home.html")
