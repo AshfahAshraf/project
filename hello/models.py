d660412fbfcc5d5786a70333d31e985f6a30942d
AshfahAshraf <ashfahashraf@gmail.com>
2026-03-10 17:08:18 +0530

    Updated  project files

--- hello/models.py
+++ hello/models.py
@@ -3,18 +3,62 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
+    Name = models.CharField(max_length=200)
     Username =models.CharField(max_length=250)
     Email =models.EmailField(unique=True,null=True, blank=True)
     Phone_Number = models.CharField(max_length=20,null=True,blank=True)
+    Address =models.CharField(max_length=200)
     Password = models.CharField(max_length=250)
 
     def __str__(self):
         return self.Username
     
-class EmailOTP(models.Model):
-        email = models.EmailField()
-        otp = models.CharField(max_length=6)
+class Product(models.Model):
+     artisan = models.ForeignKey(Artisan, on_delete=models.CASCADE)
+     Product_name = models.CharField(max_length=200)
+     Actual_price = models.CharField(max_length=20)
+     Quantity = models.IntegerField()
+     Offer_price = models.CharField(max_length=10)
+     Description = models.TextField()
 
-        def __str__(self):
-            return self.email
-            
+     def __str__(self):
+          return self.Product_name
+
+class Cart(models.Model):
+     user =models.ForeignKey(User, on_delete=models.CASCADE)
+     product=models.ForeignKey(Product, on_delete=models.CASCADE)
+     quantity = models.IntegerField(default=1)
+     
+
+class Wishlist(models.Model):
+     user = models.ForeignKey(User, on_delete=models.CASCADE)
+     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
+          
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product =models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    order_date = models.DateField(auto_now_add=True)
+
+class Artisan(models.Model):  
+     huio
+
+class Complaint(models.Model):
+     
+     Fullname =models.CharField(max_length=100)
+     Email =models.EmailField()
+     Phonenumber = models.CharField(max_length=15)
+     Orderid = models.CharField(max_length=50)
+     Productname = models.CharField(max_length=100)
+     Issue_type = models.CharField(max_length=100)
+     Product_image =models.ImageField(upload_to="complaints/",null=True, blank=True)
+     Description = models.TextField()
+     created_at =models.DateTimeField(auto_now_add=True)
+
+
+     def __str__(self):
+          return self.Fullname
+
+         
--- hello/views.py
+++ hello/views.py
@@ -6,7 +6,9 @@ from django.db.models import Q
 import random
 from django.core.mail import send_mail
 
-
+# contact
+from django.contrib import messages
+from .models import Complaint
 
 # Create your views here.
 
@@ -61,7 +63,7 @@ def index(request):
         
 
     return render(request, "register.html")
-
+########
 #email 
 
 # send otp 
@@ -71,9 +73,16 @@ def send_otp(request):
     if request.method == "POST":
         email = request.POST["email"]
 
+
+        #check if email exists
+        if not User.objects.filter(Email=email).exists():
+            return render(request, "send_otp.html",{"error": "Email not registered"})
+
         otp = str(random.randint(100000, 999999))
 
-        EmailOTP.objects.create(email =email, otp =otp)
+        #store in session instead of database
+        request.session['email'] = email
+        request.session['otp'] =otp
 
         send_mail(
             "Password Reset OTP",
@@ -83,7 +92,7 @@ def send_otp(request):
             fail_silently=False,
         )
 
-        request.session['email'] = email
+        
 
         return redirect("verify_otp")
     
@@ -96,16 +105,15 @@ def verify_otp(request):
 
     if request.method == "POST":
         user_otp = request.POST["otp"]
-        email = request.session.get("email")
+       
 
-        otp_record = EmailOTP.objects.filter(email=email, otp=user_otp).first()
 
-        if otp_record:
-            request.session["otp_verified"] = True
-            otp_record.delete()
+        if user_otp == request.session.get("otp_verifedwokring?"):
+            request.session["otp_verifed"] =True
             return redirect("reset_password")
+           
         else:
-            print("invalid OTP")
+           return render(request, "verify_otp.html", {"error": "Invalid OTP"})
 
     return render(request, "verify_otp.html")
 
@@ -128,7 +136,7 @@ def reset_password(request):
         return redirect("register")
     
     return render(request,"reset_password.html")
-
+#########
 def terms_conditon(request):
     return render(request, "terms_conditon.html")
 
@@ -148,6 +156,43 @@ def aboutus(request):
     return render(request, "aboutUs.html")
 
 def contact(request):
+
+    if request.method =="POST":
+
+        fullname = request.POST["fullname"]
+        email = request.POST["email"]
+        phonenumber = request.POST["phonenumber"]
+        orderid = request.POST["orderid"]
+        productname =request.POST["productname"]
+        issue_type = request.POST["issue_type"]
+        description = request.POST["description"]
+        product_image = request.FILES["product_image"]
+
+
+        Complaint.objects.create(
+            Fullname =fullname,
+            Email = email,
+            Phonenumber = phonenumber,
+            Orderid = orderid,
+            Productname = productname,
+            Issue_type = issue_type,
+            Product_image = product_image,
+            Description = description
+        )
+
+        #confirmation email
+        send_mail(
+            "Complaint Received",
+            f"Hello  {fullname},\n\nYour complaint has been received successfully. Our support team will contact you soon.\n\nThank you,",
+            "supoort@yourwebsite.com",
+            [email],
+            fail_silently=False
+        )
+
+        messages.success(request,"Complaint submitted successfully!")
+
+        return redirect("contact")
+
     return render(request,"contact.html")
 
 def cart(request):
